15b41577f187dd921ac7742e788af63d67ae19d0
Caspar Karsten <chukya97@gmail.com>
2020-01-16 13:52:55 +0100

    Teleportation and water

---
+++ Assets/Scripts/WaterScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    public float MainScrollX = 0.1f;
+    public float MainScrollY = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MainOffsetX = Time.time * MainScrollX;
+        float MainOffsetY = Time.time * MainScrollY;
+        GetComponent<Renderer> ().material.mainTextureOffset = new Vector2 (MainOffsetX, MainOffsetY);
+    }
+}
