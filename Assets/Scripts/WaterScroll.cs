6b84735ae755762858f760061983684c56f9d542
Caspar Karsten <chukya97@gmail.com>
2020-01-16 20:20:49 +0100

    Restored the original water somewhat

--- Assets/Scripts/WaterScroll.cs
+++ Assets/Scripts/WaterScroll.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class WaterScroll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
     public float MainScrollX = 0.1f;
     public float MainScrollY = 0.1f;
 
-    // Update is called once per frame
     void Update()
     {
         float MainOffsetX = Time.time * MainScrollX;
