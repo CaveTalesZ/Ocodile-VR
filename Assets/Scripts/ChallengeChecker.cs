db2a30ff98c32071494c24f06308227a74ea2a40
Caspar Karsten <chukya97@gmail.com>
2020-01-21 15:37:03 +0100

    Bird challenge and audio

--- Assets/Scripts/ChallengeChecker.cs
+++ Assets/Scripts/ChallengeChecker.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class ChallengeChecker : MonoBehaviour
 {
     public List<string> Solutions = new List<string>();
+    public AudioSource PlayerSound;
+    public AudioSource SolvedSound;
+    public AudioSource NotSolvedSound;
+    public AudioSource NigelSound;
+    bool ChallengeSolved = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+     AudioSource[] Sounds = GetComponents<AudioSource>();
+     PlayerSound = Sounds[0];
+     SolvedSound = Sounds[1];
+     NotSolvedSound = Sounds[2];
+     NigelSound = Sounds[3];
     }
 
     // Update is called once per frame
@@ -24,7 +33,35 @@ public class ChallengeChecker : MonoBehaviour
         {
             if(collision.gameObject.name == item)
             {
+                if (ChallengeSolved == false)
+                {
+                NotSolvedSound.Stop();
+                ChallengeSolved = true;
                 Debug.Log("Coolio");
+                SolvedSound.Play();
+                }
+            }
+            else if(collision.gameObject.name == "OVRPlayerController")
+            {
+                if (ChallengeSolved == false)
+                {
+                Debug.Log("Hello");
+                PlayerSound.Play();
+                NotSolvedSound.Stop();
+                }
+            }
+            else if(collision.gameObject.name == "Nigel")
+            {
+                Debug.Log("Hi Nigel");
+                NigelSound.Play();
+            }
+            else
+            {
+                if (ChallengeSolved == false)
+                {
+                Debug.Log("Wrong");
+                NotSolvedSound.Play();
+                }
             }
         }
             
